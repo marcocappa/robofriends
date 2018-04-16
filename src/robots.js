fb504c83838136e757d42be235d5ea825315b00a
Marco <marcocappa85@gmail.com>
2018-04-16 23:07:48 +0200

    adding first react component

---
+++ src/robots.js
@@ -0,0 +1,17 @@
+export const robots = [
+  {
+    id: 1,
+    name: 'Marco',
+    email: 'marco@prova.it'
+  },
+  {
+    id: 2,
+    name: 'Cris',
+    email: 'cris@prova.it'
+  },
+  {
+    id: 3,
+    name: 'Liam',
+    email: 'liam@prova.it'
+  },
+]
